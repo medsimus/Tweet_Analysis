32fdf4ee43b2b1255e0871b1a0bd40cd28d20462
medsimus <71640056+medsimus@users.noreply.github.com>
2022-05-24 16:52:06 +0000

    ok

--- main.py
+++ main.py
@@ -1,77 +1,64 @@
 import os
 import pandas as pd
-import time
 from datetime import datetime, timedelta
 from monfun import cnx,set_ip
 from twifun import get_data,get_data2
 from keep_run import keep_alive
 
-mongod_connect = os.environ['cnx']
-consumer_key        =os.environ['api_key']
-consumer_secret     =os.environ['api_secret']
-access_token        =os.environ['access_token']
-access_token_secret =os.environ['access_secret']
-bearer_token        =os.environ['bearer_token']
-atlas_group_id = os.environ['project_id']
-atlas_api_key_public = os.environ['public_key']
-atlas_api_key_private = os.environ['private_key']
+mongod_connect = os.environ['CNX']
+consumer_key        =os.environ['API_KEY']
+consumer_secret     =os.environ['API_SECRET']
+access_token        =os.environ['ACCESS_TOKEN']
+access_token_secret =os.environ['ACCESS_SECRET']
+bearer_token        =os.environ['BEARER_TOKEN']
+atlas_group_id = os.environ['PROJECT_ID']
+atlas_api_key_public = os.environ['PUBLIC_KEY']
+atlas_api_key_private = os.environ['PRIVATE_KEY']
 
 #twitter arg 
 keyword = "inwi (lang:fr OR lang:ar)"
 max_results = 100
 
 keep_alive()
+#cnx to mongodb
+deleteAfterDate=(datetime.now()+timedelta(hours=3,minutes=5)).isoformat()
+set_ip(atlas_group_id, atlas_api_key_public, atlas_api_key_private,deleteAfterDate)
+ctweet=cnx(mongod_connect)
+#get twitter data:
+end_time= datetime.now().strftime("%Y-%m-%dT%H:")+"00:00Z"
+tweets_data = get_data(keyword,end_time,max_results,bearer_token)
+df = pd.json_normalize(tweets_data) 
+since_id_new = tweets_data[-1]['id']
 
-while True:
-  #cnx to mongodb
-  deleteAfterDate=(datetime.now()+timedelta(hours=3,minutes=5)).isoformat()
-  set_ip(atlas_group_id, atlas_api_key_public, atlas_api_key_private,deleteAfterDate)
-  ctweet=cnx(mongod_connect)
-  #get twitter data:
-  end_time= datetime.now().strftime("%Y-%m-%dT%H:")+"00:00Z"
-  tweets_data = get_data(keyword,end_time,max_results,bearer_token)
-  df = pd.json_normalize(tweets_data) 
-  since_id_new = tweets_data[-1]['id']
-  
-  clx={}
-  for i in tweets_data:
-      try:
-        clx[i["id"]]=i
-        #print(i["created_at"])
-      except Exception as e:
-          print(e)
-          pass
-  
-  since_id_old = 0
-  while(since_id_new != since_id_old):
+clx={}
+for i in tweets_data:
     try:
-      since_id_old = since_id_new
-      tweets_data = get_data2(keyword,since_id_new,max_results,bearer_token)
-      since_id_new = tweets_data[-1]['id']
-      for i in tweets_data:
-          try:
-            clx[i["id"]]=i
-            #print(i["created_at"])
-          except:
-              pass
+        clx[i["id"]]=i
+      #print(i["created_at"])
+    except Exception as e:
+        print(e)
+        pass
+
+since_id_old = 0
+while(since_id_new != since_id_old):
+    try:
+        since_id_old = since_id_new
+        tweets_data = get_data2(keyword,since_id_new,max_results,bearer_token)
+        since_id_new = tweets_data[-1]['id']
+        for i in tweets_data:
+            try:
+              clx[i["id"]]=i
+              #print(i["created_at"])
+            except:
+                pass
     except:
-      pass
-  
-  print('New records',len(clx))
-  
-  for x in clx:
-    ctweet.delete_one({ "id": x })
-    ctweet.insert_one(clx[x])
-  
-  print('collection',ctweet.count_documents({}))
-  print('Done at ',datetime.now().strftime("%Y-%m-%dT%H:%M"))
-  time.sleep(10800)
-#for document in ctweet.find():
-#  clx_old[document['id']]=document
-#  del clx_old[document['id']]['_id']
-#clx_old.update(clx_new)
-  
+        pass
 
+print('New records',len(clx))
 
+for x in clx:
+  ctweet.delete_one({ "id": x })
+  ctweet.insert_one(clx[x])
 
-      
+print('collection',ctweet.count_documents({}))
+print('Done at ',datetime.now().strftime("%Y-%m-%dT%H:%M"))
--- monfun.py
+++ monfun.py
@@ -2,16 +2,17 @@ import pymongo
 from pymongo import MongoClient
 import requests
 from requests.auth import HTTPDigestAuth
-from ipify import get_ip
 from datetime import datetime
-
+def get_ip():
+    response = requests.get('https://api64.ipify.org?format=json').json()
+    return response["ip"]
 def cnx(mongod_connect):
-  client = MongoClient(mongod_connect)
-  db = client.dbt 
-  ctweet = db.ctweet
-  #ctweet.drop()
-  ctweet.create_index([("id", pymongo.ASCENDING)],unique = True)
-  return ctweet
+    client = MongoClient(mongod_connect)
+    db = client.dbt 
+    ctweet = db.clx
+    #ctweet.drop()
+    ctweet.create_index([("id", pymongo.ASCENDING)],unique = True)
+    return ctweet
 
 def set_ip(atlas_group_id, atlas_api_key_public, atlas_api_key_private, deleteAfterDate):
   resp = requests.post( "https://cloud.mongodb.com/api/atlas/v1.0/groups/{atlas_group_id}/accessList".format(atlas_group_id=atlas_group_id),
@@ -20,6 +21,6 @@ def set_ip(atlas_group_id, atlas_api_key_public, atlas_api_key_private, deleteAf
              'deleteAfterDate': deleteAfterDate,
              'comment': 'replit :'+str(datetime.now().strftime("%Y-%m-%dT%H"))}])
   if resp.status_code in (200, 201):
-      print("MongoDB Atlas accessList request successful", flush=True)
+        print("MongoDB Atlas accessList request successful", flush=True)
   else:
       print("Request problem: status code : {status_code}, content : {content}".format(status_code=resp.status_code, content=resp.content),flush=True)
